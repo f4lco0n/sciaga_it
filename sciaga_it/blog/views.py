52fcdce11f830dd870e2a3e1af93266f4e2d1b08
f4lco0n <pawel_sokol@o2.pl>
2020-11-11 18:18:33 +0100

    add django forms to CreateView

--- sciaga_it/blog/views.py
+++ sciaga_it/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 class HomeView(ListView):
@@ -14,5 +15,6 @@ class PostDetailView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = "add_post.html"
-    fields = '__all__'
+    # fields = '__all__'
