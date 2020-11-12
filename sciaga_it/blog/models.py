bc5e37e4c93297ac6c98cdcbff2035309e88b32c
sz0n <kamilrsw@gmail.com>
2020-11-12 21:22:01 +0100

    adddelateView

--- sciaga_it/blog/models.py
+++ sciaga_it/blog/models.py
@@ -9,10 +9,10 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
-
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
         # return reverse('post-details', args=(str(self.id)))
         return reverse('home')
+
