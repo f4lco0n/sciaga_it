3cc132172c37a6cb82795d83ad7187c8254732e0
f4lco0n <pawel_sokol@o2.pl>
2020-11-15 18:03:44 +0100

    fix problem with displaying category in forms

--- sciaga_it/blog/forms.py
+++ sciaga_it/blog/forms.py
@@ -27,11 +27,18 @@ class PostForm(forms.ModelForm):
 class UpdatePostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag','category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
+        labels = {
+            "title": "Tytuł ściągi",
+            "title_tag": "Tag ściągi",
+            "category": "Kategoria",
+            "body": "Zawartość"
         }
