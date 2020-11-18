60499d7ce119331457ba7c05590d65c7439fbd91
f4lco0n <pawel_sokol@o2.pl>
2020-11-18 22:35:31 +0100

    remove author field from PostForm

--- sciaga_it/blog/forms.py
+++ sciaga_it/blog/forms.py
@@ -11,7 +11,8 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control','value':'',
+                                             'id': 'author_input','type': 'hidden'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
 
         }
