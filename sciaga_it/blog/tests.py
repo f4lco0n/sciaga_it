ad2454677f5551a2a3d6fb3ca045e1e5799a244c
f4lco0n <pawel_sokol@o2.pl>
2020-12-17 22:37:44 +0100

    add tests for post's views

--- sciaga_it/blog/tests.py
+++ sciaga_it/blog/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+from .views import HomeView, PostDetailView, PostListView, \
+    PostCreateView, UpdatePostView, DeletePostView, category_view, \
+    show_user_post_view, show_user_profile_view
 
-# Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func.view_class, HomeView)
+
+    def test_post_details_resolves(self):
+        url = reverse('post-details', args=[1])
+        self.assertEquals(resolve(url).func.view_class, PostDetailView)
+
+    def test_post_lists_resolves(self):
+        url = reverse('post_list')
+        self.assertEquals(resolve(url).func.view_class, PostListView)
+
+    def test_add_post_resolves(self):
+        url = reverse('add_post')
+        self.assertEquals(resolve(url).func.view_class, PostCreateView)
+
+    def test_edit_post_resolves(self):
+        url = reverse('update-post', args=[1])
+        self.assertEquals(resolve(url).func.view_class, UpdatePostView)
+
+    def test_delete_post_resolves(self):
+        url = reverse('delete-post', args=[1])
+        self.assertEquals(resolve(url).func.view_class, DeletePostView)
+
+    def test_category_view_resolves(self):
+        url = reverse('category', args=['programowanie'])
+        self.assertEquals(resolve(url).func, category_view)
+
+    def test_user_post_view_resolves(self):
+        url = reverse('user_posts', args=['f4lco0n'])
+        self.assertEquals(resolve(url).func, show_user_post_view)
+
+    def test_user_profile_view_resolves(self):
+        url = reverse('user_profile', args=['f4lco0n'])
+        self.assertEquals(resolve(url).func, show_user_profile_view)
