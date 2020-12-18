932f2f5bccf3a9027925dc2938868612d1e58ab8
f4lco0n <pawel_sokol@o2.pl>
2020-12-18 19:39:00 +0100

    add test for tutorials urls

--- sciaga_it/blog/tests.py
+++ sciaga_it/blog/tests.py
@@ -2,10 +2,14 @@ from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 from .views import HomeView, PostDetailView, PostListView, \
     PostCreateView, UpdatePostView, DeletePostView, category_view, \
-    show_user_post_view, show_user_profile_view
+    show_user_post_view, show_user_profile_view, TutorialListView,\
+    TutorialDetailView, TutorialCreateView, TutorialUpdateView,\
+    DeleteTutorialView
 
 
 class TestUrls(SimpleTestCase):
+    # POSTS
+
     def test_home_url_resolves(self):
         url = reverse('home')
         self.assertEquals(resolve(url).func.view_class, HomeView)
@@ -41,3 +45,25 @@ class TestUrls(SimpleTestCase):
     def test_user_profile_view_resolves(self):
         url = reverse('user_profile', args=['f4lco0n'])
         self.assertEquals(resolve(url).func, show_user_profile_view)
+
+    # TUTORIALS
+
+    def test_tutorial_list_resolves(self):
+        url = reverse('tutorial_list')
+        self.assertEquals(resolve(url).func.view_class, TutorialListView)
+
+    def test_tutorial_details_resolves(self):
+        url = reverse('tutorial-details', args=[1])
+        self.assertEquals(resolve(url).func.view_class, TutorialDetailView)
+
+    def test_add_tutorial_resolves(self):
+        url = reverse('add_tutorial')
+        self.assertEquals(resolve(url).func.view_class, TutorialCreateView)
+
+    def test_update_tutorial_resolves(self):
+        url = reverse('update-tutorial', args=[1])
+        self.assertEquals(resolve(url).func.view_class, TutorialUpdateView)
+
+    def test_delete_tutorial_resolves(self):
+        url = reverse('delete-tutorial', args=[1])
+        self.assertEquals(resolve(url).func.view_class, DeleteTutorialView)
