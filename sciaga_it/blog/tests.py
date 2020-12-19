e828cbcc639dfd9eff22f13b0c7164432a1014ca
f4lco0n <pawel_sokol@o2.pl>
2020-12-19 23:12:43 +0100

    add view test for post CreateView

--- sciaga_it/blog/tests.py
+++ sciaga_it/blog/tests.py
@@ -1,9 +1,12 @@
 from django.test import SimpleTestCase
+from django.test import TestCase, Client
+from .models import Category, Post, User
+import json
 from django.urls import resolve, reverse
 from .views import HomeView, PostDetailView, PostListView, \
     PostCreateView, UpdatePostView, DeletePostView, category_view, \
-    show_user_post_view, show_user_profile_view, TutorialListView,\
-    TutorialDetailView, TutorialCreateView, TutorialUpdateView,\
+    show_user_post_view, show_user_profile_view, TutorialListView, \
+    TutorialDetailView, TutorialCreateView, TutorialUpdateView, \
     DeleteTutorialView
 
 
@@ -67,3 +70,46 @@ class TestUrls(SimpleTestCase):
     def test_delete_tutorial_resolves(self):
         url = reverse('delete-tutorial', args=[1])
         self.assertEquals(resolve(url).func.view_class, DeleteTutorialView)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name='programowanie'
+        )
+        self.user = User.objects.create(
+            username='testuser',
+            password='testpassword'
+        )
+        self.post1 = Post.objects.create(
+            title='sciaga1',
+            category=self.category,
+            title_tag='#1',
+            author=self.user,
+            short_description='opis testowy',
+            body='dlugi opis testowy',
+            is_private=0
+        )
+
+    def test_post_create_POST(self):
+        url = reverse('add_post')
+        response = self.client.post(url, {
+            'title': 'sciaga2',
+            'category': 1,
+            'title_tag': 'sciaga #2',
+            'author': 1,
+            'short_description': 'druga sciaga',
+            'body': 'opis sciagi drugiej',
+            'is_private': 0,
+
+        })
+
+        post2 = Post.objects.get(id=2)
+        # assert if created post is saved correctly
+        self.assertEquals(post2.title, 'sciaga2')
+        self.assertEquals(post2.is_private, 0)
+        self.assertEquals(post2.body, 'opis sciagi drugiej')
+        category_test = Category.objects.get(id=1)
+        # assert if category is saved correctly
+        self.assertEquals(category_test.id, post2.category_id)
+        self.assertEquals(category_test.name, 'programowanie')
